2a895fa62550b70ec049f186ada68bd00a363bd7
ayabelkhiri <a.belkhiri@esi-sba.dz>
2024-05-05 22:28:22 +0100

    custom Bar

---
+++ lib/views/widgets/custom_app_bar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:notes/views/widgets/custom_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+         Text(
+          'Notes' ,
+           style: TextStyle(
+          fontSize: 28,
+        ),
+        ),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
